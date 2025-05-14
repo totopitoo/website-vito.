cd72bb0932739186e27c6b31427f364a62fcb8b0
totopitoo <158444744+totopitoo@users.noreply.github.com>
2025-05-14 07:08:16 +0700

    Add files via upload

---
+++ script.js
@@ -0,0 +1,8 @@
+//toggle class active
+const navbarNav = document.querySelector('.navbar-nav');
+//ketika navbar hamburger menu di klik
+document.querySelector('#hamburger-menu'),
+  (onclick = () => {
+    navbarNav.classList.toggle('active');
+  }
+);
